f4c45830a7142fa1d1991d84a791a6cb10a9295c
Vishal Minz <vishalm@mindfiresolutions.com>
2026-04-08 18:33:04 +0530

    eslint and prettier setup

---
+++ prettier.congig.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 150,
+  tabWidth: 2
+};
+
+export default config;
