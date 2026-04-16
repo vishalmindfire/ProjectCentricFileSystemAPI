c6245d7a2ef86df6b24fa47b4a815224b663d815
Vishal Minz <vishalm@mindfiresolutions.com>
2026-04-16 13:42:52 +0530

    changes for ssl

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { createApp } from '#app.js';
 import pool from '#config/dbConnector.js';
 import httpsOptions from '#config/httpsConfig.js';
+import http from 'http';
 import https from 'https';
 
 const app = createApp();
 const httpsPort = process.env.HTTPS_PORT ?? '3000';
-
 try {
   console.log('connecting to db');
   const client = await pool.connect();
@@ -14,8 +14,14 @@ try {
 } catch (error: unknown) {
   console.error('Error connecting to the database:', error);
 }
-
-const httpsServer = https.createServer(httpsOptions, app);
-httpsServer.listen(httpsPort, () => {
-  console.log(`HTTPS server listening on port ${httpsPort}`);
-});
+if (process.env.NODE_ENV === 'production') {
+  const httpsServer = https.createServer(httpsOptions, app);
+  httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS server listening on port ${httpsPort}`);
+  });
+} else {
+  const httpServer = http.createServer(app);
+  httpServer.listen(httpsPort, () => {
+    console.log(`HTTP server listening on port ${httpsPort}`);
+  });
+}
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,18 +4,21 @@ import { RequestHandler } from 'express';
 const allowedOrigins = [
   'https://www.cpfsystem.com:5173',
   'https://www.cpfsystem.local:4000',
-  'https://127.0.0.1:5173',
   'https://localhost:5173',
   'http://www.cpfsystem.com:5173',
   'http://www.cpfsystem.local:4000',
-  'http://127.0.0.1:5173',
   'http://localhost:5173',
+  'http://www.cpfsystem.com:4173',
+  'http://localhost:4173',
+  'https://project-centric-file-system.vercel.app/',
+  'https://project-centric-file-system-api.vercel.app/',
 ];
 const corsOptions: CorsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void): void => {
+    console.log(origin);
     if (!origin) {
       callback(null, true);
       return;
